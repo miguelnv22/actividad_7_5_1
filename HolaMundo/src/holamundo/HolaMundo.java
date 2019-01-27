f0ff8466a120cf8b81ccd44abbd5c0c763422bc4
miguelnv22 <miguelnv22@gmail.com>
2019-01-27 17:49:38 +0100

    Segunda modificación en rama issue54

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -6,6 +6,7 @@ public class HolaMundo {
         System.out.println("Hola mundo.");
         System.out.println("Saludos mundo.");
         System.out.println("Mod uno");
+        System.out.println("Mod dos");
     }
     
 }
